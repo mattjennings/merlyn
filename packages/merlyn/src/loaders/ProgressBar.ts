3e6547c0d1985a9c11fcc71c509e8f93a2b356c2
mattjennings <me@mattjennings.io>
2022-07-26 17:18:11 -0500

    dont use ex namespace in ProgressBar

--- packages/merlyn/src/loaders/ProgressBar.ts
+++ packages/merlyn/src/loaders/ProgressBar.ts
@@ -1,36 +1,37 @@
-import { Actor } from 'excalibur'
+import type { ActorArgs } from 'excalibur'
+import { Actor, Canvas, Color, Util } from 'excalibur'
 
 export class ProgressBar extends Actor {
   progress = 0
 
-  constructor(args: ex.ActorArgs) {
+  constructor(args: ActorArgs) {
     super(args)
   }
 
   onInitialize() {
-    const canvas = new ex.Canvas({
+    const canvas = new Canvas({
       width: this.width,
       height: this.height,
       quality: window.devicePixelRatio,
       draw: (ctx) => {
-        ex.Util.DrawUtil.roundRect(
+        Util.DrawUtil.roundRect(
           ctx,
           0,
           0,
           this.width,
           this.height,
           this.height / 2,
-          ex.Color.White
+          Color.White
         )
-        ex.Util.DrawUtil.roundRect(
+        Util.DrawUtil.roundRect(
           ctx,
           0,
           0,
           this.width * Math.max(0.01, Math.min(1, this.progress / 100)),
           this.height,
           this.height / 2,
-          ex.Color.White,
-          ex.Color.White
+          Color.White,
+          Color.White
         )
       },
     })
